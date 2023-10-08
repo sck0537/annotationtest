9be003ff76c98ecdf6c00d30c7942c808597a9dc
sck <sckzen@gmail.com>
2023-10-09 03:06:45 +0800

    moving

---
+++ plusweb/src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/quick")
+    public String save(){
+        System.out.println("controller save running");
+        return "success.jsp";
+
+    }
+}
